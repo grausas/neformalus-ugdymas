4650b9a8d33cf68eaf21c1cf87f5a383cece045b
Marius Pikzirnis <grausas@gmail.com>
2023-09-04 17:41:34 +0300

    added card data with icons

---
+++ src/utils/categoryData.ts
@@ -0,0 +1,101 @@
+import technologijos from "../assets/technologijos.svg";
+import kalbos from "../assets/kalbos.svg";
+import medijos from "../assets/medijos.svg";
+import choreografija from "../assets/choreografija.svg";
+import daile from "../assets/daile.svg";
+import etnokultura from "../assets/etnokultura.svg";
+import gamta from "../assets/gamta.svg";
+import muzika from "../assets/muzika.svg";
+import pilietiskumas from "../assets/pilietiskumas.svg";
+import saugus_eismas from "../assets/saugus_eismas.svg";
+import sportas from "../assets/sportas.svg";
+import drama from "../assets/drama.svg";
+import technine_kuryba from "../assets/technine_kuryba.png";
+import turizmas from "../assets/turizmas.svg";
+
+export const CategoryData = [
+  {
+    id: 1,
+    value: 1,
+    text: "Choreografija, šokis",
+    icon: choreografija,
+  },
+  {
+    id: 2,
+    value: 2,
+    text: "Dailė",
+    icon: daile,
+  },
+  {
+    id: 3,
+    value: 3,
+    text: "Etnokultūra",
+    icon: etnokultura,
+  },
+  {
+    id: 4,
+    value: 4,
+    text: "Gamta, ekologija",
+    icon: gamta,
+  },
+  {
+    id: 5,
+    value: 5,
+    text: "Informacinės technologijos",
+    icon: technologijos,
+  },
+  {
+    id: 6,
+    value: 6,
+    text: "Kalbos",
+    icon: kalbos,
+  },
+  {
+    id: 7,
+    value: 7,
+    text: "Medijos",
+    icon: medijos,
+  },
+  {
+    id: 8,
+    value: 8,
+    text: "Muzika",
+    icon: muzika,
+  },
+  {
+    id: 9,
+    value: 9,
+    text: "Pilietiškumas",
+    icon: pilietiskumas,
+  },
+  {
+    id: 10,
+    value: 10,
+    text: "Saugus eismas",
+    icon: saugus_eismas,
+  },
+  {
+    id: 11,
+    value: 11,
+    text: "Sportas",
+    icon: sportas,
+  },
+  {
+    id: 12,
+    value: 12,
+    text: "Teatras, drama",
+    icon: drama,
+  },
+  {
+    id: 13,
+    value: 13,
+    text: "Techninė kūryba",
+    icon: technine_kuryba,
+  },
+  {
+    id: 14,
+    value: 14,
+    text: "Turizmas ir kraštotyra",
+    icon: turizmas,
+  },
+];
