8911fd9b3e1d42c2d06b11c4d2e9135ffcfbf4f8
Marius Pikzirnis <grausas@gmail.com>
2023-09-04 15:17:19 +0300

    added hero to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,41 @@
-import { Button, Box } from "@chakra-ui/react";
+"use client";
+
+import {
+  Box,
+  Heading,
+  Container,
+  Text,
+  Button,
+  Stack,
+  Icon,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <>
-      <Button bg="brand.30">Hello</Button>
+      <Container maxW={"3xl"}>
+        <Stack
+          as={Box}
+          textAlign={"center"}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+            lineHeight={"110%"}
+          >
+            Neformalus <br />
+            <Text as={"span"} color={"green.400"}>
+              ugdymas
+            </Text>
+          </Heading>
+          <Text color={"gray.500"}>
+            VISOS VEIKLOS VAIKAMS IR JAUNIMUI VILNIAUS MIESTE VIENOJE VIETOJE
+          </Text>
+        </Stack>
+      </Container>
     </>
   );
 }
