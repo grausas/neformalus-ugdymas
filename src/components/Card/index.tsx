c62aa5f6e1d49cab73cbaaae71ae13fe615d50ba
Marius Pikzirnis <grausas@gmail.com>
2023-09-04 17:41:51 +0300

    changed style to card, added related icons

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { Flex, Text, Heading, Box } from "@chakra-ui/react";
+import { Flex, Text, Heading, Box, Tooltip, Link } from "@chakra-ui/react";
+import { CategoryData } from "@/utils/categoryData";
+import Image from "next/image";
+import { EmailIcon, PhoneIcon, ExternalLinkIcon } from "@chakra-ui/icons";
 
 export default function Card({ cardData }: { cardData: __esri.Graphic }) {
   return (
@@ -7,14 +10,50 @@ export default function Card({ cardData }: { cardData: __esri.Graphic }) {
       <Heading size="md" color="brand.50">
         {cardData.attributes.PAVADIN}
       </Heading>
-      <Box color="brand.40">
-        <Text>{cardData.attributes.EL_PASTAS}</Text>
+      <Box color="brand.40" mt="2">
         <Text>{cardData.attributes.ADRESAS}</Text>
-        <Text>{cardData.attributes.NUORODA}</Text>
+        <Flex alignItems="center">
+          <EmailIcon mr="2" color="brand.30" />
+          <Text>{cardData.attributes.EL_PASTAS}</Text>
+        </Flex>
+        <Flex alignItems="center">
+          <ExternalLinkIcon mr="2" color="brand.30" />
+          <Link href={`http://${cardData.attributes.NUORODA}`} isExternal>
+            {cardData.attributes.NUORODA}
+          </Link>
+        </Flex>
         <Text>{cardData.attributes.PASTABA}</Text>
         <Text>{cardData.attributes.SOC_TINKL}</Text>
+        <Flex alignItems="center">
+          <PhoneIcon mr="2" color="brand.30" />
+          <Text>+{cardData.attributes.TELEF_MOB}</Text>
+        </Flex>
         <Text>{cardData.attributes.TELEFONAS}</Text>
-        <Text>{cardData.attributes.TELEF_MOB}</Text>
+        <Flex justifyContent="flex-end" mt="1">
+          {cardData.attributes.relatedFeatures.map((related: any) => {
+            return CategoryData.map((category) => {
+              if (related.attributes.LO_VEIKLA === category.value) {
+                return (
+                  <Flex justify="center" key={category.id} ml="2">
+                    <Tooltip
+                      label={category.text}
+                      fontSize="xs"
+                      bg="brand.30"
+                      color="brand.50"
+                    >
+                      <Image
+                        width={28}
+                        height={28}
+                        src={category.icon}
+                        alt={category.text}
+                      />
+                    </Tooltip>
+                  </Flex>
+                );
+              }
+            });
+          })}
+        </Flex>
       </Box>
     </Flex>
   );
