1814de3b6b0bc4cfa79dd905322f5c8a158a6d66
Marius Pikzirnis <grausas@gmail.com>
2023-09-07 15:24:43 +0300

    related table added

--- src/layers.ts
+++ src/layers.ts
@@ -22,3 +22,14 @@ export const featureLayerPrivate = () => {
   });
   return layer;
 };
+
+export const featureLayerPrivateTable = () => {
+  const layer = new FeatureLayer({
+    url: "https://opencity.vplanas.lt/arcgis/rest/services/P_Vaiku_ugdymas/Vaiku_ugdymas_edit/FeatureServer/1",
+    outFields: ["*"],
+    title: "Jaunimo ugdymas",
+    id: "private-table",
+    effect: "drop-shadow(0px, 0px, 3px)",
+  });
+  return layer;
+};
