b2d6f7176ec0c35972fc19c0e1c574c5ff9d2ec1
Marius Pikzirnis <grausas@gmail.com>
2023-09-07 15:24:30 +0300

    added types to related data

--- src/types/form.ts
+++ src/types/form.ts
@@ -7,4 +7,14 @@ export type FormValues = {
   TELEFONAS: string;
   SOC_TINKL: string;
   PASTABA: string;
+  related: {
+    LO_VEIKLA: number;
+    KLASE_1_4: number;
+    KLASE_5_8: number;
+    KLASE_9_12: number;
+    NVS_KREPSE: number;
+    SPC_POREIK: number;
+    PEDAGOGAS: string;
+    PASTABA: string;
+  };
 };
