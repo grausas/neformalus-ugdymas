cc78e6a08820451b6d40e64da4627bc15f267830
Marius Pikzirnis <grausas@gmail.com>
2023-09-07 13:32:15 +0300

    added layer to edit

--- src/layers.ts
+++ src/layers.ts
@@ -1,3 +1,4 @@
+"use client";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
 export const featureLayerPublic = () => {
@@ -6,6 +7,17 @@ export const featureLayerPublic = () => {
     outFields: ["*"],
     title: "Jaunimo ugdymas",
     id: "public",
+    effect: "drop-shadow(0px, 0px, 3px)",
+  });
+  return layer;
+};
+
+export const featureLayerPrivate = () => {
+  const layer = new FeatureLayer({
+    url: "https://opencity.vplanas.lt/arcgis/rest/services/P_Vaiku_ugdymas/Vaiku_ugdymas_edit/FeatureServer/0",
+    outFields: ["*"],
+    title: "Jaunimo ugdymas",
+    id: "private",
     effect: "drop-shadow(0px, 0px, 3px)",
   });
   return layer;
