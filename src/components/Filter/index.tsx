3b964686c81cd3ddd8ef41482308ed48d000c26b
Marius Pikzirnis <grausas@gmail.com>
2023-09-11 10:28:41 +0300

    added more filters

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -7,12 +7,24 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
+  Stack,
 } from "@chakra-ui/react";
 import { CategoryData } from "@/utils/categoryData";
 
 type FilterProps = {
   handleFilter: (category: string[]) => void;
 };
+
+const NVS = [
+  { value: 1, text: "Taikomas" },
+  { value: 2, text: "Netaikomas" },
+];
+
+const classData = [
+  { value: 1, text: "1-4 klasė" },
+  { value: 1, text: "5-8 klasė" },
+  { value: 1, text: "9-12 klasė" },
+];
 
 function Filter({ handleFilter }: FilterProps) {
   const [category, setCategory] = useState<string[]>([]);
@@ -40,26 +52,68 @@ function Filter({ handleFilter }: FilterProps) {
 
   return (
     <FormControl>
-      <Menu closeOnSelect={false}>
-        <MenuButton as={Button} size="sm" bg="brand.10">
-          Veiklos
-        </MenuButton>
-        <MenuList>
-          {CategoryData.map((item, index) => {
-            return (
-              <MenuItem key={item.id} py="1">
-                <Checkbox
-                  value={item.value}
-                  onChange={(e) => handleChange(e, index)}
-                  size="sm"
-                >
-                  {item.text}
-                </Checkbox>
-              </MenuItem>
-            );
-          })}
-        </MenuList>
-      </Menu>
+      <Stack direction="row">
+        <Menu closeOnSelect={false}>
+          <MenuButton as={Button} size="sm" bg="brand.10">
+            Veiklos
+          </MenuButton>
+          <MenuList>
+            {CategoryData.map((item, index) => {
+              return (
+                <MenuItem key={item.id} py="1">
+                  <Checkbox
+                    value={item.value}
+                    onChange={(e) => handleChange(e, index)}
+                    size="sm"
+                  >
+                    {item.text}
+                  </Checkbox>
+                </MenuItem>
+              );
+            })}
+          </MenuList>
+        </Menu>
+        <Menu closeOnSelect={false}>
+          <MenuButton as={Button} size="sm" bg="brand.10">
+            NVŠ krepšelis
+          </MenuButton>
+          <MenuList>
+            {NVS.map((item, index) => {
+              return (
+                <MenuItem key={index} py="1">
+                  <Checkbox
+                    value={item.value}
+                    // onChange={(e) => handleChange(e, index)}
+                    size="sm"
+                  >
+                    {item.text}
+                  </Checkbox>
+                </MenuItem>
+              );
+            })}
+          </MenuList>
+        </Menu>
+        <Menu closeOnSelect={false}>
+          <MenuButton as={Button} size="sm" bg="brand.10">
+            Klasės
+          </MenuButton>
+          <MenuList>
+            {classData.map((item, index) => {
+              return (
+                <MenuItem key={index} py="1">
+                  <Checkbox
+                    value={item.value}
+                    // onChange={(e) => handleChange(e, index)}
+                    size="sm"
+                  >
+                    {item.text}
+                  </Checkbox>
+                </MenuItem>
+              );
+            })}
+          </MenuList>
+        </Menu>
+      </Stack>
     </FormControl>
   );
 }
