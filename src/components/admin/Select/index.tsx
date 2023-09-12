f7916844030093b687c66e296d69a4ffb0c1fe7d
Marius Pikzirnis <grausas@gmail.com>
2023-09-12 13:58:11 +0300

    select multiple option

--- src/components/admin/Select/index.tsx
+++ src/components/admin/Select/index.tsx
@@ -1,11 +1,18 @@
 import {
+  Button,
+  Checkbox,
   FormControl,
   FormErrorMessage,
   FormLabel,
   InputGroup,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Select,
 } from "@chakra-ui/react";
 import React from "react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 type Props = {
   register: any;
@@ -35,22 +42,34 @@ export default function SelectField({
       <FormLabel htmlFor={id} m="0" fontSize="sm" color="brand.40">
         {name}
       </FormLabel>
-      <InputGroup>
-        {children}
-        <Select
-          id={id}
-          placeholder={placeholder}
-          {...register(registerValue, options)}
-          bg="brand.10"
+      <Menu closeOnSelect={false}>
+        <MenuButton
+          as={Button}
+          variant="outline"
+          rightIcon={<ChevronDownIcon />}
+          w="100%"
+          fontSize="sm"
+          fontWeight="400"
+          rounded="md"
         >
+          Pasirinkti veiklas
+        </MenuButton>
+        <MenuList>
           {selectOptions &&
             selectOptions.map((option) => (
-              <option key={option.id} value={option.value}>
-                {option.text}
-              </option>
+              <MenuItem key={option.id}>
+                <Checkbox
+                  value={option.value}
+                  size="sm"
+                  id={id}
+                  {...register(registerValue, options)}
+                >
+                  {option.text}
+                </Checkbox>
+              </MenuItem>
             ))}
-        </Select>
-      </InputGroup>
+        </MenuList>
+      </Menu>
       <FormErrorMessage>{error}</FormErrorMessage>
     </FormControl>
   );
