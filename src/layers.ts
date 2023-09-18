30a09b9f3030d951d3672f20ede903e0cab463e3
Marius Pikzirnis <grausas@gmail.com>
2023-09-18 15:45:00 +0300

    renamed layers

--- src/layers.ts
+++ src/layers.ts
@@ -12,6 +12,17 @@ export const featureLayerPublic = () => {
   return layer;
 };
 
+export const featureLayerPublicTable = () => {
+  const layer = new FeatureLayer({
+    url: "https://opencity.vplanas.lt/arcgis/rest/services/P_Vaiku_ugdymas/Vaiku_ugdymas/MapServer/1",
+    outFields: ["*"],
+    title: "Jaunimo ugdymas",
+    id: "private-table",
+    effect: "drop-shadow(0px, 0px, 3px)",
+  });
+  return layer;
+};
+
 export const featureLayerPrivate = () => {
   const layer = new FeatureLayer({
     url: "https://opencity.vplanas.lt/arcgis/rest/services/P_Vaiku_ugdymas/Vaiku_ugdymas_edit/FeatureServer/0",
@@ -29,6 +40,17 @@ export const featureLayerPrivateTable = () => {
     outFields: ["*"],
     title: "Jaunimo ugdymas",
     id: "private-table",
+    effect: "drop-shadow(0px, 0px, 3px)",
+  });
+  return layer;
+};
+
+export const featureLayerForRenderer = () => {
+  const layer = new FeatureLayer({
+    url: "https://opencity.vplanas.lt/arcgis/rest/services/P_Vaiku_ugdymas/Vaiku_ugdymas/MapServer/2",
+    outFields: ["*"],
+    title: "Jaunimo ugdymas",
+    id: "renderer",
     effect: "drop-shadow(0px, 0px, 3px)",
   });
   return layer;
