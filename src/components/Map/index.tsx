0400b903a57bf57f96963a5182eef2c08cfec3ae
Marius Pikzirnis <grausas@gmail.com>
2023-09-19 13:52:31 +0300

    changed map layer

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -3,14 +3,12 @@
 import { useContext, useEffect, useRef } from "react";
 import { Box } from "@chakra-ui/react";
 import { MapContext } from "../../context/map-context";
-import { AuthContext } from "@/context/auth";
-import { featureLayerPublic, featureLayerPrivate } from "../../layers";
+import { featureLayerForRenderer } from "../../layers";
 
 const ArcGISMap = () => {
   const mapRef = useRef<HTMLDivElement>(null);
   const { loadMap } = useContext(MapContext);
-  const auth = useContext(AuthContext);
-  const layer = auth.user.token ? featureLayerPrivate() : featureLayerPublic();
+  const layer = featureLayerForRenderer();
 
   useEffect(() => {
     if (mapRef.current && loadMap) {
