1f9e281ce744438dd6494d34f25207fe966ea1f0
Marius Pikzirnis <grausas@gmail.com>
2023-09-21 10:55:03 +0300

    added label to icons for legend renderer

--- src/helpers/layerRenderer.ts
+++ src/helpers/layerRenderer.ts
@@ -6,20 +6,69 @@ const symbol = (url: string, width: string, height: string) => ({
 });
 
 const uniqueValueInfos = [
-  { value: "1", symbol: symbol("sportas.svg", "23px", "23px") },
-  { value: "2", symbol: symbol("technologijos.svg", "23px", "23px") },
-  { value: "3", symbol: symbol("drama.svg", "23px", "23px") },
-  { value: "4", symbol: symbol("kalbos.svg", "23px", "23px") },
-  { value: "5", symbol: symbol("choreografija.svg", "23px", "23px") },
-  { value: "6", symbol: symbol("daile.svg", "23px", "23px") },
-  { value: "7", symbol: symbol("muzika.svg", "23px", "23px") },
-  { value: "8", symbol: symbol("turizmas.svg", "23px", "23px") },
-  { value: "9", symbol: symbol("etnokultura.svg", "23px", "23px") },
-  { value: "10", symbol: symbol("gamta.svg", "23px", "23px") },
-  { value: "11", symbol: symbol("technine_kuryba.png", "23px", "23px") },
-  { value: "12", symbol: symbol("pilietiskumas.svg", "23px", "23px") },
-  { value: "13", symbol: symbol("saugus_eismas.svg", "23px", "23px") },
-  { value: "14", symbol: symbol("medijos.svg", "23px", "23px") },
+  {
+    value: "1",
+    label: "Sportas",
+    symbol: symbol("sportas.svg", "23px", "23px"),
+  },
+  {
+    value: "2",
+    label: "Informacinės technologijos",
+    symbol: symbol("technologijos.svg", "23px", "23px"),
+  },
+  {
+    value: "3",
+    label: "Teatras, drama",
+    symbol: symbol("drama.svg", "23px", "23px"),
+  },
+  { value: "4", label: "Kalbos", symbol: symbol("kalbos.svg", "23px", "23px") },
+  {
+    value: "5",
+    label: "Choreografija, šokis",
+    symbol: symbol("choreografija.svg", "23px", "23px"),
+  },
+  { value: "6", label: "Dailė", symbol: symbol("daile.svg", "23px", "23px") },
+  { value: "7", label: "Muzika", symbol: symbol("muzika.svg", "23px", "23px") },
+  {
+    value: "8",
+    label: "Turizmas ir kraštotyra",
+    symbol: symbol("turizmas.svg", "23px", "23px"),
+  },
+  {
+    value: "9",
+    label: "Etnokultūra",
+    symbol: symbol("etnokultura.svg", "23px", "23px"),
+  },
+  {
+    value: "10",
+    label: "Gamta, ekologija",
+    symbol: symbol("gamta.svg", "23px", "23px"),
+  },
+  {
+    value: "11",
+    label: "Techninė kūryba",
+    symbol: symbol("technine_kuryba.png", "23px", "23px"),
+  },
+  {
+    value: "12",
+    label: "Pilietiškumas",
+    symbol: symbol("pilietiskumas.svg", "23px", "23px"),
+  },
+  {
+    value: "13",
+    label: "Saugus eismas",
+    symbol: symbol("saugus_eismas.svg", "23px", "23px"),
+  },
+  {
+    value: "14",
+    label: "Medijos",
+    symbol: symbol("medijos.svg", "23px", "23px"),
+  },
+  {
+    value: "15",
+    label: "Kitos",
+    symbol: symbol("medijos.svg", "23px", "23px"),
+  },
 ];
 
 export const simpleRenderer = {
